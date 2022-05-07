da0efe60113c4917bcf2fade31ff7b050f65d893
ajiths10 <ajithsnair10@gmail.com>
2022-05-07 23:31:45 +0530

    spread operator

--- first-app.js
+++ first-app.js
@@ -1,7 +1,10 @@
 const arr = ['apple','orange','mango','','lemon'];
-newarr = [];
+
 
 const array= arr.filter((ele,i)=>{
      return ele !==''
 })
 console.log(array)
+
+const newarr =[...array,'cherry']
+console.log(newarr)
