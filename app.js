fba29c70a4f76208a2e590923d3f58ea0b4143a3
ajiths10 <ajithsnair10@gmail.com>
2022-05-10 20:35:09 +0530

    routing

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const http = require('http');
-
+const fs =require('fs');
+const { url } = require('inspector');
 
 const server = http.createServer((req , res)=>{
     console.log(req.url , req.method);
+    const method = req.method;
+    const url = req.url;
     //process.exit();
 
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>My First Page</title></head>');
-        if(req.url=='/Home'){
+        if(url=='/Home'){
         res.write('<body><h1> Welcome home</h1><body>');
         }
-        if(req.url=='/about'){
+        if(url=='/about'){
         res.write('<body><h1>Welcome to About Us page</h1><body>');
         }
-        if(req.url=='/node'){
+        if(url=='/node'){
         res.write('<body><h1>Welcome to my Node Js project</h1><body>');
+        }
+        if(url=='/'){
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type=submit>Submit</button></form><body>');
+        }
+        if(url === '/message' && method === 'POST'){
+            fs.writeFileSync('message.txt','Dummy')
+            res.statusCode = 302;
+            res.setHeader('Location' ,'/')
+            return res.end();
         }
         res.write('</html>');
     res.end();
