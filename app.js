3fe216e7c36167e95372dc8bafa0b76b35692a4b
ajiths10 <ajithsnair10@gmail.com>
2022-05-14 16:33:40 +0530

    request , routing , parsing in express js

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const http = require('http');
 
 const express = require('express');
+const bodyPaser = require('body-parser');
 
 const app =express();
 
-app.use((req, res , next)=> {
-    console.log('Hello world App');
-    next(); //next allow request to countiue to the next middleware 
+app.use(bodyPaser.urlencoded({extended:false}));
+
+app.use('/addproduct',(req, res , next)=> {   
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Product name"><br><input type="number" name="size" placeholder="size"><br><button type="submit">Add Product</button> </form>');
 })
-app.use((req, res , next)=> {
-    console.log('Another Hello world App');   
+
+app.use('/product',(req, res , next)=> {
+    console.log(req.body)
+    res.redirect('/');
+})
+
+app.use('/',(req, res , next)=> {   
     res.send('<h1>Hello World Express!</h1>');
 })
 
 const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
