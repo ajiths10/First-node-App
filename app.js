5150ec75fbca7a8761ef5e3e0f70686bc696de31
ajiths10 <ajithsnair10@gmail.com>
2022-05-14 15:51:48 +0530

    express js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const app =express();
+
+app.use((req, res , next)=> {
+    console.log('Hello world App');
+    next(); //next allow request to countiue to the next middleware 
+})
+app.use((req, res , next)=> {
+    console.log('Another Hello world App');   
+    res.send('<h1>Hello World Express!</h1>');
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
